6dc6ffc41f6355604e81db7c4bef6b495a295895
aniftos <aniftos@gmail.com>
2022-03-29 23:26:33 +0100

    set python version more relazed

--- run_tests.sh
+++ run_tests.sh
@@ -5,10 +5,9 @@ pip install poetry
 poetry install
 #poetry export -f requirements.txt --output requirements.txt --without-hashes
 #pip install -r requirements.txt
-python -m pytest -m "unit" --cov=training --cov-config=./coverage.ini \
+poetry run pytest -m "unit" --cov=training --cov-config=./coverage.ini \
 --cov-report=term-missing --cov-fail-under=60 -s -v -c custom_pytest.ini
-python -m pytest -m "inte" --cov=training --cov-config=./coverage.ini \
---cov-report=term-missing --cov-fail-under=10 -s -v -c custom_pytest.ini
+#poetry run pytest -m "inte" -s -v -c custom_pytest.ini
 rtrn_code=$?
 coverage erase
 deactivate
